60e860d71a6253ce281362f9995dc15f95404ed6
tunny17 <tvnji17@gmail.com>
2023-07-04 04:25:58 +0100

    Svelte tutorial

---
+++ src/AddPersonForm.svelte
@@ -0,0 +1,42 @@
+<script>
+    import { createEventDispatcher } from "svelte";
+    
+    let dispatch = createEventDispatcher();
+    
+    let age;
+    let name;
+    let beltColor;
+    let skills = [];
+
+    const handleSubmit = () => {
+        const person = {
+            name,
+            beltColor,
+            age, 
+            skills,
+            id: Math.random(),
+        };
+        dispatch('addPerson', person);
+    }
+</script>
+
+<form on:submit|preventDefault={handleSubmit}>
+    <input type="text" placeholder="name" bind:value={name}>
+    <input type="number" placeholder="Age" bind:value={age}>
+    <label for=''>Skills:</label>
+    <input type="checkbox" bind:group={skills} value="fighting">Fighting<br>
+    <input type="checkbox" bind:group={skills} value="sneaking">Sneaking<br>
+    <input type="checkbox" bind:group={skills} value="running">Running<br>
+    <label for="">Belt Color</label>
+    <select bind:value={beltColor}>
+        <option value="black">black</option>
+        <option value="orange">orange</option>
+        <option value="red">red</option>
+        <option value="brown">brown</option>
+    </select>
+    <button>Add Person</button>
+</form>
+
+<style>
+
+</style>
--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,57 @@
 <script>
-	export let name;
+	import Modal from './Modal.svelte';
+	import AddPersonForm from './AddPersonForm.svelte';
+	
+	let showModal = false;
+
+	let people = [
+		{ name: 'Ramy', beltColor: 'red', age: 25, id: 1 },
+		{ name: 'Sonny', beltColor: 'blue', age: 65, id: 2 },
+		{ name: 'Ahmed', beltColor: 'purple', age: 55, id: 3 },
+	]
+
+	const handleClick = (id) => {
+		// delete person from people
+		people = people.filter((person) => person.id != id);
+	}
+
+	const toggleModal = () => {
+		showModal = !showModal;
+	}
+
+	const addPerson = (e) => {
+		// console.log(e.detail);
+		const person = e.detail;
+		people = [person, ...people];
+		showModal = false;
+	}
+
 </script>
 
+<Modal {showModal} on:click={toggleModal}>
+	<AddPersonForm on:addPerson={addPerson} />
+</Modal>
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<button on:click={toggleModal}>Open Modal</button>
+	{#each people as person (person.id)}
+		<div>
+			<h4>{person.name}</h4>
+			{#if person.beltColor === 'red'}
+				<p><strong>Master Ninja</strong></p>
+			{:else if person.beltColor === 'blue'}
+				<p><strong>Sensi</strong></p>
+			{:else}
+				<p><strong>Student</strong></p>
+			{/if}
+			<p>{person.age} years old, {person.beltColor}</p>
+			{#if person.skills}
+				<p>{person.skills}</p>
+			{/if}
+			<button on:click={() => handleClick(person.id)}>Delete</button>
+		</div>
+	{:else}
+		<p>There are no people to show...</p>
+	{/each}
 </main>
 
 <style>
@@ -13,13 +60,6 @@
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
 	}
 
 	@media (min-width: 640px) {
