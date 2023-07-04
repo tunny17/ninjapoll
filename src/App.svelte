a55724f0996d384d34351eab49f52c15c75060e4
tunny17 <tvnji17@gmail.com>
2023-07-04 05:01:45 +0100

    Added the tab component

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,27 @@
 <script>
     import Footer from "./components/Footer.svelte";
 	import Header from "./components/Header.svelte";
+	import Tabs from "./shared/Tabs.svelte";
+	
+
+	// tabs
+	let items = ['Current Polls', 'Add New Poll'];
+	let activeItem = 'Current Polls';
+
+	const tabChange = (e) => {
+		activeItem = e.detail;
+	}
 
 </script>
 
 <Header />
 <main>
-	
+	<Tabs {items} {activeItem} on:tabChange={tabChange}/>
+	{#if activeItem === 'Current Polls'}
+		<p>Poll List</p>
+	{:else if activeItem === 'Add New Poll'}
+		<p>Add new poll</p>
+	{/if}
 </main>
 <Footer />
 
